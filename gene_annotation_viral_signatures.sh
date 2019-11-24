794f9747ea66aa3ee0addaffbcdfb45edc6752d1
Ashley <44369148+ash-bell@users.noreply.github.com>
2019-11-24 14:14:04 +0000

    Update gene_annotation_viral_signatures.sh

--- gene_annotation_viral_signatures.sh
+++ gene_annotation_viral_signatures.sh
@@ -5,3 +5,27 @@ diamond blastx -d <diamond_nr_database> \
 -o <diamond_output> \
 --outfmt 6 qseqid evalue bitscore stitle -k 1 \
 --more-sensitive
+
+```{r}
+library(ggfittext)
+library(gggenes)
+library(ggplot2)
+library(ggrepel)
+library(RColorBrewer)
+
+virus <- read.csv("<VIRSorter_diamond_output>", sep = "\t")
+
+colourCount = length(unique(virus$COG_cat))
+getPalette = colorRampPalette(brewer.pal(9, "Set1"))
+
+ggplot(virus, aes(xmin = Start, xmax = End, y = Genome, fill = COG_cat, forward = Strand, label = role)) +
+  geom_gene_arrow(arrowhead_height = unit(10, "mm"), arrowhead_width = unit(3, "mm"), arrow_body_height = unit(10, "mm") ) +
+  geom_gene_label(align = "middle") +
+  facet_wrap(~ Genome, scales = "free", ncol = 1) +
+  scale_fill_manual(values = colorRampPalette(brewer.pal(9, "Set1"))(colourCount)) +
+  theme_genes() +
+  theme(legend.position="bottom") +
+  guides(fill=guide_legend(nrow=5))
+
+ggsave("<gggenes_output>", width = 420, height = 297, units = "mm", limitsize = FALSE, dpi = 300)
+```
